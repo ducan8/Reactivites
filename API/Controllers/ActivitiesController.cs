bcd82b4b790f7199bc9fd63fa226d055c9e31008
ducan8 <ducan9c@gmail.com>
2024-10-17 16:49:15 +0700

    create API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using SQLitePCL;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseAPIController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            if(activity == null) { return null; }
+            return activity;
+        }
+    }
+}
